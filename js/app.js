d4f5c9d94dd9575d49fca62737fdf09d2974c2bc
Daniel Robinson <djr.self@gmail.com>
2015-04-29 00:03:23 -0400

    Add form submission behaviour

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ var app = angular.module('app', []);
 
 app.controller('appController', ['$scope', function ($scope) {
   
+  var _default_tax = 8;
   $scope.meal = {}
   $scope.meal.base = 100;
-  $scope.meal.tax = 12;
   $scope.meal.tip = 19;
+  $scope.meal.tax = _default_tax;
+
 
   $scope.earnings = {}
   $scope.earnings.tipTotal = 0;
@@ -20,6 +22,46 @@ app.controller('appController', ['$scope', function ($scope) {
   };
 
   $scope.subtotal = function(){
-    return $scope.meal.base + $scope.meal.base * $scope.meal.tax / 100;
+    if ($scope.meal.base == undefined || $scope.meal.tax == undefined) {
+      return 0;
+    } else {
+      return $scope.meal.base + $scope.meal.base * $scope.meal.tax / 100;
+    };
   };
+
+  $scope.submitMeal = function(){
+    if (isEmpty($scope.newMeal.$error)) {
+      $scope.earnings.tipTotal += $scope.meal.tip;
+      $scope.earnings.mealCount += 1;
+      this.cancel();
+      pristifyForm();
+    }
+  };
+
+  $scope.cancel = function(){
+    $scope.meal = { tax: _default_tax };
+    pristifyForm();
+  };
+
+  $scope.resetAll = function(){
+    $scope.earnings = {};
+    this.cancel();
+    pristifyForm();
+
+  };
+
+  function pristifyForm(){
+    $scope.newMeal.$setPristine();
+    $scope.newMeal.$setUntouched();
+  };
+
+  function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+
+    return true;
+  }
+
 }]);
